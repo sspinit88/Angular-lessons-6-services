80af329db4d42fbfdddda535ce3306c1ec0c9c48
Sergey Pashnin <s.pashnin@encry.ru>
2018-11-16 17:27:38 +0300

    fin

--- src/app/cars.service.ts
+++ src/app/cars.service.ts
@@ -1,9 +1,16 @@
 import {Injectable} from '@angular/core';
+// импортируем сервис
+import {ConsoleService} from './console.service';
 
+// этот декоратор позволяет импортиоровать в сервис различные сущности
 @Injectable({
     providedIn: 'root'
 })
 export class CarsService {
+
+    // инициализируем ранее созданный сервис
+    constructor(private consoleService: ConsoleService) {
+    }
 
     // выносим ранее созданный массив с данными из app.component.ts
     cars = [
@@ -26,5 +33,6 @@ export class CarsService {
             isSold: false,
             name: name
         });
+        this.consoleService.log(`Машина ${name} добавлена! (service-in-service)`);
     }
 }
